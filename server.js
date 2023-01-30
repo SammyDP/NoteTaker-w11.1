2c0cfa8a822e3c3e8050ee128837295901a3f910
SammyDP <107449948+SammyDP@users.noreply.github.com>
2023-01-30 08:29:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
     
 app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
